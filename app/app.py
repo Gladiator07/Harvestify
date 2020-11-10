b7ed9e7ef91fe6d3ffc0f7772a58d6192cdccd94
Gladiator07 <atharvaaingle@gmail.com>
2020-11-10 12:20:28 +0530

    added placeholders in app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 
 # Load the Random Forest CLassifier model
-filename = 'diabetes-prediction-rfc-model.pkl'
+filename = '../Trained_Model/XGBoost.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 
 app = Flask(__name__)
