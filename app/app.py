1b405df8be4066f1dc4b59eb81a80526cdef7fd2
Gladiator07 <atharvaaingle@gmail.com>
2020-11-18 16:28:42 +0530

    flask integration

--- app/app.py
+++ app/app.py
@@ -27,23 +27,23 @@ def disease_prediction():
 	return render_template('disease.html')
 
 	
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     if request.method == 'POST':
-#         N = int(request.form['nitrogen'])
-#         P = int(request.form['phosphorous'])
-#         K = int(request.form['pottasium'])
-#         temperature = float(request.form['temperature'])
-#         humidity = float(request.form['humidity'])
-#         ph = float(request.form['ph'])
-#         rainfall = float(request.form['rainfall'])
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        temperature = float(request.form['temperature'])
+        humidity = float(request.form['humidity'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
         
         
-#         data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
-#         my_prediction = classifier.predict(data)
-#         final_prediction = my_prediction[0]
+        data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+        my_prediction = classifier.predict(data)
+        final_prediction = my_prediction[0]
         
-#         return render_template('result.html', prediction=final_prediction)
+        return render_template('result.html', prediction=final_prediction)
 
 if __name__ == '__main__':
 	app.run(debug=True)
