6caa0ab3dcf3f1d2e059c807680dd690228803ec
Gladiator07 <atharvaaingle@gmail.com>
2020-11-19 16:11:13 +0530

    added live weather api

--- app/app.py
+++ app/app.py
@@ -2,12 +2,29 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-
+import requests, json
 # Load the Random Forest CLassifier model
 filename = '../Trained_Model/RandomForest.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 
 app = Flask(__name__)
+
+def weather_fetch(city_name):
+    api_key = "9d7cde1f6d07ec55650544be1631307e"
+    base_url  = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+
+        return temperature, humidity
+
 
 @app.route('/')
 def home():
@@ -33,19 +50,24 @@ def crop_prediction():
         N = int(request.form['nitrogen'])
         P = int(request.form['phosphorous'])
         K = int(request.form['pottasium'])
-        # temperature = float(request.form['temperature'])
-        # humidity = float(request.form['humidity'])
-        # ph = float(request.form['ph'])
-        # rainfall = float(request.form['rainfall'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
         
         
+        
+        state = request.form.get("stt")
+        city = request.form.get("city")
+        temperature, humidity = weather_fetch(city)
         # data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
         # my_prediction = classifier.predict(data)
         # final_prediction = my_prediction[0]
-        state = request.form.get("stt")
-        city = request.form.get("city")
 
-        return render_template('crop_result.html', nitrogen=N, phosphorous=P, pottasium=K, state=state, city=city)
+        return render_template('crop_result.html', nitrogen=N, phosphorous=P, pottasium=K, state=state, city=city, temperature=temperature, humidity=humidity, ph=ph, rainfall=rainfall)
+        
+
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- app_test/weather.py
+++ app_test/weather.py
@@ -14,6 +14,7 @@ complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 
 response = requests.get(complete_url)
 x = response.json()
+print(x)
 
 if x["cod"] != "404":
     y = x["main"]
@@ -21,13 +22,13 @@ if x["cod"] != "404":
     current_temperature = round((y["temp"] - 273.15), 2)
     current_pressure = y["pressure"]
     current_humidity = y["humidity"]
-    z = x["weather"]
+    rainfall = y["precipitation"]
 
-    weather_description = z[0]["description"]
 
-    print("Temperature (in degree celsius) = " + str(current_temperature))
-    print("Atmospheric Pressure (in hPa unit) = " + str(current_pressure))
-    print("Humidity (in percentage) = " + str(current_humidity))
+    print("Rainfall (in degree celsius) = " + str(rainfall))
+
+    # print("Atmospheric Pressure (in hPa unit) = " + type(current_pressure))
+    # print("Humidity (in percentage) = " + type(current_humidity))
     
 else:
     print("City not found")
