8c5939a0b73344fbc10ed76bb504c6f56aabb59a
Gladiator07 <atharvaaingle@gmail.com>
2020-12-08 23:25:03 +0530

    refactored classes and modules

--- app/model.py
+++ app/model.py
@@ -1,46 +1,8 @@
-import os # for working with files
-import torch # Pytorch module 
-import matplotlib.pyplot as plt # for plotting informations on graph and images using tensors
-import torch.nn as nn # for creating  neural networks
-from torch.utils.data import DataLoader # for dataloaders 
-from PIL import Image # for checking images
-import torch.nn.functional as F # for functions for calculating loss
-import torchvision.transforms as transforms # for transforming images into tensors 
-from torchvision.utils import make_grid # for data checking
-from torchvision.datasets import ImageFolder # for working with classes and images
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
 
-def accuracy(outputs, labels):
-    _, preds = torch.max(outputs, dim=1)
-    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-    
 
-class DiseaseClassificationBase(nn.Module):
-    
-    def training_step(self, batch):
-        images, labels = batch
-        out = self(images)
-        loss = F.cross_entropy(out, labels)
-        return loss
-    
-    def validation_step(self, batch):
-        images, labels = batch
-        out = self(images)
-        loss = F.cross_entropy(out, labels)
-        accur = accuracy(out, labels)
-        return {"val_loss": loss.detach(), "val_accuracy": accur}
-    
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x["val_loss"] for x in outputs]
-        batch_accuracy = [x["val_accuracy"] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()
-        epoch_accuracy = torch.stack(batch_accuracy).mean()
-        return {"val_loss": epoch_loss, "val_accuracy": epoch_accuracy}
-    
-    def epoch_end(self, epoch, result):
-        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
-            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_accuracy']))
-
-# Architecture for training
 def ConvBlock(in_channels, out_channels, pool=False):
     layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
              nn.BatchNorm2d(out_channels),
@@ -50,7 +12,8 @@ def ConvBlock(in_channels, out_channels, pool=False):
     return nn.Sequential(*layers)
 
 
-class ResNet9(DiseaseClassificationBase):
+# Model Architecture
+class ResNet9(nn.Module):
     def __init__(self, in_channels, num_diseases):
         super().__init__()
         
